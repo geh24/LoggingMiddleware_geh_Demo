e71e08023a5fcc8ea2b4b55d62112ab541302f80
Gerhard Herre <geh@gmx.de>
2022-08-07 13:35:57 +0200

    mit LoggingMiddleware

--- LoggingMiddleware_geh_Demo/Program.cs
+++ LoggingMiddleware_geh_Demo/Program.cs
@@ -1,9 +1,15 @@
+using LoggingMiddleware;
+using NLog;
+
 namespace LoggingMiddleware_geh_Demo
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            LoggingMiddleware.LoggingMiddleware.init("nlog.config");
+            Logger log = LoggingMiddleware.LoggingMiddleware.getLoger();
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -21,14 +27,24 @@ namespace LoggingMiddleware_geh_Demo
                 app.UseSwaggerUI();
             }
 
+            LoggingMiddleware.LoggingMiddleware.enableTrace(true);
+            app.UseLoggingMiddleware();
+
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
-            app.Run();
+            try {
+                log.Info("init main");
+                app.Run();
+                log.Info("shutdown main");
+            } catch (Exception e) {
+                //NLog: catch setup errors
+                log.Error(e, "Stopped program because of exception");
+                throw;
+            } finally {
+                NLog.LogManager.Shutdown();
+            }
         }
     }
 }
